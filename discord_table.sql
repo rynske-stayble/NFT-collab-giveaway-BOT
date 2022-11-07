182061df5169f9641f96bbc5bdf7b97859f6b57c
rynske-stayble <72295840+rynske-stayble@users.noreply.github.com>
2022-11-07 16:38:12 +0900

    Update discord_table.sql

--- discord_table.sql
+++ discord_table.sql
@@ -3,6 +3,9 @@
 -- serversというテーブルを作成
 -- column_name data_type, column_name data_type, ...
 -- 主キー: Discord server ID
+-- server_id: Discord server ID, template_message: 自分のNFTの宣伝文, post_channel_id: 他のNFTの宣伝文を投稿するchannelのID
+-- start_post_at: 投稿開始日時, post_interval: 投稿間隔, plan: 登録中のプラン（無料プラン、有料プランなど）, register_at: BOTへの認証日時
+-- posted_templates_servers: 宣伝文を相互投稿したNFTのDiscordサーバー
 CREATE TABLE servers (
     server_id varchar(100) primary key,
     template_message TEXT,
